adce0c21c59bbfdf82d6c0c32207c6ce4e78180e
BergerZvika <59172440+BergerZvika@users.noreply.github.com>
2020-01-24 01:31:27 +0200

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/ex4.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/zvi/Desktop/ex4/CacheManager.cpp" "/home/zvi/Desktop/ex4/cmake-build-debug/CMakeFiles/ex4.dir/CacheManager.cpp.o"
+  "/home/zvi/Desktop/ex4/FileCacheManager.cpp" "/home/zvi/Desktop/ex4/cmake-build-debug/CMakeFiles/ex4.dir/FileCacheManager.cpp.o"
+  "/home/zvi/Desktop/ex4/MyClientHandler.cpp" "/home/zvi/Desktop/ex4/cmake-build-debug/CMakeFiles/ex4.dir/MyClientHandler.cpp.o"
+  "/home/zvi/Desktop/ex4/MyParallelServer.cpp" "/home/zvi/Desktop/ex4/cmake-build-debug/CMakeFiles/ex4.dir/MyParallelServer.cpp.o"
+  "/home/zvi/Desktop/ex4/MySerialServer.cpp" "/home/zvi/Desktop/ex4/cmake-build-debug/CMakeFiles/ex4.dir/MySerialServer.cpp.o"
+  "/home/zvi/Desktop/ex4/MyTestClientHandler.cpp" "/home/zvi/Desktop/ex4/cmake-build-debug/CMakeFiles/ex4.dir/MyTestClientHandler.cpp.o"
+  "/home/zvi/Desktop/ex4/ObjectAdapter.cpp" "/home/zvi/Desktop/ex4/cmake-build-debug/CMakeFiles/ex4.dir/ObjectAdapter.cpp.o"
+  "/home/zvi/Desktop/ex4/SearcheableMatrix.cpp" "/home/zvi/Desktop/ex4/cmake-build-debug/CMakeFiles/ex4.dir/SearcheableMatrix.cpp.o"
+  "/home/zvi/Desktop/ex4/StringReverser.cpp" "/home/zvi/Desktop/ex4/cmake-build-debug/CMakeFiles/ex4.dir/StringReverser.cpp.o"
+  "/home/zvi/Desktop/ex4/Vertax.cpp" "/home/zvi/Desktop/ex4/cmake-build-debug/CMakeFiles/ex4.dir/Vertax.cpp.o"
+  "/home/zvi/Desktop/ex4/main.cpp" "/home/zvi/Desktop/ex4/cmake-build-debug/CMakeFiles/ex4.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/ex4.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ex4.dir/CacheManager.cpp.o"
+  "CMakeFiles/ex4.dir/FileCacheManager.cpp.o"
+  "CMakeFiles/ex4.dir/MyClientHandler.cpp.o"
+  "CMakeFiles/ex4.dir/MyParallelServer.cpp.o"
+  "CMakeFiles/ex4.dir/MySerialServer.cpp.o"
+  "CMakeFiles/ex4.dir/MyTestClientHandler.cpp.o"
+  "CMakeFiles/ex4.dir/ObjectAdapter.cpp.o"
+  "CMakeFiles/ex4.dir/SearcheableMatrix.cpp.o"
+  "CMakeFiles/ex4.dir/StringReverser.cpp.o"
+  "CMakeFiles/ex4.dir/Vertax.cpp.o"
+  "CMakeFiles/ex4.dir/main.cpp.o"
+  "ex4"
+  "ex4.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ex4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
